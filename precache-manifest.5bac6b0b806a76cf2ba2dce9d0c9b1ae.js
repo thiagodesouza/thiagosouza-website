c2b03a5f9563be392eef0274550a1344b538dc8b
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-01-05 06:36:24 +0000

    Deploy thiagodesouza/thiagosouza-website to github.com/thiagodesouza/thiagosouza-website.git:gh-pages

---
+++ precache-manifest.5bac6b0b806a76cf2ba2dce9d0c9b1ae.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2e8487db2384bd27b92243140129b79",
+    "url": "/CNAME"
+  },
+  {
+    "revision": "a8d5a7eec3951d53e779",
+    "url": "/css/app.b14d2de9.css"
+  },
+  {
+    "revision": "9553a36600a158db3d7492d700602e76",
+    "url": "/fonts/Icons.9553a366.ttf"
+  },
+  {
+    "revision": "2505bfbd9bde14a7829cc8c242a0d25c",
+    "url": "/fonts/Ubuntu-Regular.2505bfbd.ttf"
+  },
+  {
+    "revision": "261be354e09603be1a040daac56ba00f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a8d5a7eec3951d53e779",
+    "url": "/js/app.18470b10.js"
+  },
+  {
+    "revision": "d5026b41b9051570b3b0",
+    "url": "/js/chunk-vendors.4cb98585.js"
+  },
+  {
+    "revision": "5997d683a9123819289f099c4694d3f6",
+    "url": "/manifest.json"
+  }
+]);
